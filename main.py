f29062347568d51f8c534428e40d2c68e789e3c5
arthur <arthur.broudoux@gmail.com>
2024-09-19 17:09:10 +0200

    feat: parameters during program launching Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import argparse
+
 from src.game import Game
 
+
+def main():
+    parser = argparse.ArgumentParser(description="Options for Game")
+
+    parser.add_argument("--cheat", type=bool, default=False, help="Enable cheat mode")
+    parser.add_argument("--random", type=bool, default=False, help="Enable random mode")
+    parser.add_argument("--max_len", type=int, default=4, help="Set maximum word length")
+
+    args = parser.parse_args()
+
+    game = Game(cheat=args.cheat, random=args.random, max_len=args.max_len)
+    game.play()
+
 if __name__ == '__main__':
-    game = Game(cheat=False, random=False, max_len=4).play()
+    main()
